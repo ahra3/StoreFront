f8df136b79591d8379ca6047e1ed3d96f1532030
ZAHRA MAROUF <z.marouf@esi-sba.dz>
2025-03-20 01:08:52 +0100

    end of Advanced API part=add pagination, filters, and search

--- store/views.py
+++ store/views.py
@@ -1,21 +1,25 @@
-from itertools import count
 from django.db.models import Count
-from django.shortcuts import get_object_or_404
-from django.http import HttpRequest,HttpResponse
-from rest_framework.decorators import api_view
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter,OrderingFilter
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
-from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 from .models import Product,Collection,Review
+from .fliters import ProductFilter
+from .pagination import DefaultPagination
 from .serializers import ProductSerializer, CollectionSerializer,ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
+    filter_backends=[DjangoFilterBackend,SearchFilter,OrderingFilter]
+    filterset_class=ProductFilter
+    pagination_class=DefaultPagination
+    search_fields=['title','description']
+    ordering_fields=['unit_price','last_update']
+
+    
     
     def get_serializer_context(self):
         return {"request":self.request}
@@ -37,7 +41,21 @@ class CollectionViewSet(ModelViewSet):
             return Response({'error':'Collection cannot be deleted because it contains products'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
 class ReviewViewSet(ModelViewSet):
-    queryset=Review.objects.all()
     serializer_class=ReviewSerializer
-    def get_serializer_context(self):
+    
+    """
+        here we are overriding the get_serializer_context method
+        to pass the product_id to the serializer
+        
+    """
+    def get_serializer_context(self): # we extract the product_id from the views page bcz it where the url is applied 
         return {'product_id':self.kwargs['product_pk']}
+    
+    """
+        here we are overriding the get_queryset method
+        to list only the reviews of the product with the id in the url
+        
+    """
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
